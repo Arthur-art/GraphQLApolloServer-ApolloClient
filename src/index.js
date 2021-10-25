d851bfee059eb2ccb982d876972e7202c00c95d8
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-25 11:11:19 -0300

    Aprendendo mais sobre resolvers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import {ApolloServer, gql} from "apollo-server"
 const port = 4003;
 const server = new ApolloServer({
     typeDefs: gql`
+        type User {
+            name: String!
+            age: Int!
+        }
         type Query{
             hello: String!
+            user: User!
+            
         }
-    `
-    
+    `,
+    resolvers: {
+        Query:{
+            hello: () => "Hello",
+            user: () =>{
+                return {
+                    name: "Arthur",
+                    age: 25
+                }
+            }
+        }
+    }
+
 });
 
 server.listen(port).then(({url})=>{
