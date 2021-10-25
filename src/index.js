033583f4bf0d154237b311fd583b85e865039272
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-25 10:56:06 -0300

    Iniciando o server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import {ApolloServer} from "apollo-server"
+import {ApolloServer, gql} from "apollo-server"
+
+const port = 4003;
+const server = new ApolloServer({
+    typeDefs: gql`
+        type Query{
+            hello: String!
+        }
+    `
+    
+});
+
+server.listen(port).then(({url})=>{
+    console.log(`server rodando na url ${url}`)
+})
