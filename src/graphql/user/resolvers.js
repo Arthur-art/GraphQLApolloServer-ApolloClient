ead76364b28fa4689853844c00c755568fa7a34b
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-26 10:49:41 -0300

    Refatorando aplicacao passando request user por context

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,26 +1,13 @@
-const users = async (_,__, {fetch})=>{
- const users = await fetch('http://localhost:3000/users')
-    .then(res => {
-      if (res.status >= 400) {
-        throw new Error("Bad response from server");
-      }
-      return res.json();
-    })
-    .then(user => {
-      return user
-    })
-    .catch(err => {
-      console.error(err);
-    });
+const users = (_,__, {userRequest})=>{
+ 
+  const users =  userRequest;
   return users;
 }
 
-const user = async (_,args,{fetch}) =>{
-    const user = await fetch("http://localhost:3000/users")
-    .then((response)=>{
-        return response.json()
-    })
-    const filterUser = user.find((value)=>{
+const user = async (_,args,{userRequest}) =>{
+
+    const users = await userRequest;
+    const filterUser = await users.find((value)=>{
         if(value.id === args.id){
             return value;
         }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import {ApolloServer, gql} from "apollo-server"
 import { resolvers, typeDefs } from "./graphql/schema";
 import fetch from 'cross-fetch';
 
+const userRequest = fetch("http://localhost:3000/users")
+.then((response)=>{
+    return response.json()
+});
+
 const port = 4003;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context:()=>{
         return{
-            fetch
+            userRequest
         }
     }
 });
